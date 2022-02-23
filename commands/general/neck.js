64c75bedc0ed2b3f20426ecdc1622e4aa5a2637d
Gale King <49294179+nanowrimoijk@users.noreply.github.com>
2022-02-23 23:57:00 +0000

    created neck %command

---
+++ commands/general/neck.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "neck",
+    description: "starbreak neck ;)",
+    execute(client, message, args, Discord) {
+			message.channel.send("<:Starbreak_has_a_neck_1:946193462779322398>\n<:Starbreak_has_a_neck_2:946193462779322398>");
+    }
+}
