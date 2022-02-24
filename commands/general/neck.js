fa47e4ac6f11138b2582f94bdf2da05d10d872e4
Gale King <49294179+nanowrimoijk@users.noreply.github.com>
2022-02-24 23:13:44 +0000

    fixed neck command for the last time

--- commands/general/neck.js
+++ commands/general/neck.js
@@ -1,6 +1,5 @@
 module.exports = {
     name: "neck",
-		admin: true, 
     description: "starbreak neck ;)",
     execute(client, message, args, Discord) {
 			message.channel.send("<:Starbreak_has_a_neck_1:769043388417966100>\n<:Starbreak_has_a_neck_2:769043449654804510>");
