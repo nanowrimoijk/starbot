bb40afe26d948251edb4e62f7d57be71f4db1176
Gale King <49294179+nanowrimoijk@users.noreply.github.com>
2022-02-24 23:15:35 +0000

    neck is now longer

--- commands/general/neck.js
+++ commands/general/neck.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "neck",
     description: "starbreak neck ;)",
     execute(client, message, args, Discord) {
-			message.channel.send("<:Starbreak_has_a_neck_1:769043388417966100>\n<:Starbreak_has_a_neck_2:769043449654804510>");
+			message.channel.send("<:Starbreak_has_a_neck_1:769043388417966100>\n<:Starbreak_has_a_neck_2:769043449654804510>\n<:Starbreak_has_a_neck_2:769043449654804510>\n<:Starbreak_has_a_neck_2:769043449654804510>");
     }
 }
